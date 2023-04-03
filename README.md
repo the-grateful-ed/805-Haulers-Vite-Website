df9f745bc1ccf2dd33e829fac81382dd78323ede
the-grateful-ed <82444464+the-grateful-ed@users.noreply.github.com>
2023-04-03 15:39:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 805-Haulers-Vite-Website
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-3zfcyf)
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <h1 className="bg-green-700 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
